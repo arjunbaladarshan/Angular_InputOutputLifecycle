a3fd9d2a2147d65fc6d7078cd6d2860af9f672d2
arjunbaladarshan <60723978+arjunbaladarshan@users.noreply.github.com>
2025-01-02 08:56:50 +0530

    Initial test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { AboutComponent } from "./about/about.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'InputOutputLifeCycle';
+  counterNo = 0;
+  msg = "";
+
+  incrementCounter(){
+    this.counterNo++;
+  }
+
+  recievedMsg(e:any){
+    this.msg = e
+  }
 }
